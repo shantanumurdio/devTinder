cc1fa608b895b283d7bc0c0c9817c681c57c3839
Shantanu Murdio <shantanu.murdio@aitglobalinc.com>
2025-06-27 13:53:19 +0530

    schema validation

--- src/models/user.js
+++ src/models/user.js
@@ -3,22 +3,46 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required: true,
+    minLength: 4,
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    required: true,
+    lowercase: true,
+    trim:true, 
+    // unique: true,
   },
   password: {
     type: String,
   },
   age: {
     type: Number,
+    min: 18,
   },
   gender: {
     type: String,
+    validate(value){
+      if(!["male","female","other"].includes(value)){
+        throw new Error("Gender data is not valid")
+      }
+    }
   },
+  photoUrl:{
+    type: String,
+  },
+  about:{
+    type: String,
+    default: "This is the default user"
+  },
+  skills:{
+    type: [String],
+  }
+},{
+  timestamps:true,
 });
 
 // const User = mongoose.model("User", userSchema);
