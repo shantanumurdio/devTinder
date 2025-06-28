bb6bbb1369fd27dcd89610474f5266f30d6f21a8
Shantanu Murdio <shantanu.murdio@aitglobalinc.com>
2025-06-28 19:36:44 +0530

    exploring validator

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator")
 
 const userSchema = new mongoose.Schema({
   firstName: {
@@ -14,10 +15,20 @@ const userSchema = new mongoose.Schema({
     required: true,
     lowercase: true,
     trim:true, 
-    // unique: true,
+    unique: true,
+    validate(value){
+      if(!validator.isEmail(value)){
+        throw new Error("Invalid Email Address " + value)
+      }
+    }
   },
   password: {
     type: String,
+    validate(value){
+      if(!validator.isStrongPassword(value)){
+        throw new Error("Enter a strong password " + value)
+      }
+    }
   },
   age: {
     type: Number,
@@ -33,6 +44,11 @@ const userSchema = new mongoose.Schema({
   },
   photoUrl:{
     type: String,
+    validate(value){
+      if(!validator.isURL(value)){
+        throw new Error("Invalid Photo URL " + value)
+      }
+    }
   },
   about:{
     type: String,
